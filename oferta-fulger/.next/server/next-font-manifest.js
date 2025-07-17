3802e314e1ffd3694a0a96541e3c3b2482b0a802
careerswitch <abolat01@gmail.com>
2025-07-17 10:41:46 +0300

    Untrack files now ignored

--- oferta-fulger/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/workstation/projects/clones/new/viabolat/oferta-fulger/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- oferta-fulger/src/components/AuthHeaderWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { getSession } from '@/app/auth/session';
-import Header from './Header';
-
-export default async function AuthHeaderWrapper() {
-  const session = await getSession();
-  return <Header session={session} />;
-}
